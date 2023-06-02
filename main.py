98f5f46cfb0e1f421091f8bc285fa04987173fca
Diego Corredeira <91692393+DiegoCorredeira@users.noreply.github.com>
2023-06-02 16:16:45 -0300

    funcao para atualizacao de preco

--- main.py
+++ main.py
@@ -21,6 +21,32 @@ def criar_tabela():
     conn.commit()
     conn.close()
 
+def atualizar_preco(url, nome, preco, categoria):
+    conn = sqlite3.connect('precos.db')
+    cursor = conn.cursor()
+
+    cursor.execute(''' SELECT * FROM produtos WHERE url = ? ''', (url,))
+    produto = cursor.fetchone()
+
+    if produto is None:
+        cursor.execute(''' INSERT INTO produtos VALUES (?, ?, ?, ?, ?, ?) ''', (url, nome, preco, preco, preco, categoria))
+        print(f'Produto {nome} foi adicionado ao banco de dados.')
+        pb.push_note('Novo Produto!', f'Produto {nome} foi adicionado ao banco de dados.')
+    else:
+        _, _, last_price, min_price, max_price, _ = produto
+        if preco != last_price:
+            if preco < min_price:
+                min_price = preco
+                pb.push_note('O produto ficou mais barato!', f'Produto {nome} atingiu o preço {min_price:.2f}.')
+            elif preco > max_price:
+                max_price = preco
+                pb.push_note('O produto ficou mais caro!', f'Produto {nome} atingiu o preço {max_price:.2f}.')
+            cursor.execute(''' UPDATE produtos SET preco = ?, menor_preco = ?, maior_preco = ? WHERE url = ? ''', (preco, min_price, max_price, url))
+            print(f'Preço do produto {nome} foi atualizado! Acesse o banco de dados para mais detalhes.')
+    conn.commit()
+    conn.close()
+
+
 # class tit-prod 
 # class val-prod valVista
 def verifica_preco(url, categoria):
@@ -38,6 +64,8 @@ def verifica_preco(url, categoria):
 
         print(f'Produto: {nome}')
         print(f'Preço à vista: {preco:2f}')
+        atualizar_preco(url, nome, preco, categoria)
+        
     except NoSuchElementException:
         print('Elemento não encontrado')
     
